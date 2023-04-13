ac7336dcc4d99b6cd62d83aae8a972113030ef95
Santiago <glaze@outlook.es>
2023-04-12 21:26:07 -0500

    test(ActorController): create integration tests
    Creates integration test for save, findById and deleteById endpoints

--- src/main/java/com/glaze/movieapi/controller/ActorController.java
+++ src/main/java/com/glaze/movieapi/controller/ActorController.java
@@ -18,7 +18,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/v1/actor")
+@RequestMapping("/api/v1/actors")
 @RequiredArgsConstructor
 @Tag(name = "Actor", description = "All actions that are only applicable to actors")
 public class ActorController {
