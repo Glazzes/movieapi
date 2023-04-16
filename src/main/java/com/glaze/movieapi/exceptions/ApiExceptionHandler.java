5b5d9f872426c89320ea02a8e239015db0e7abb4
Santiago <glaze@outlook.es>
2023-04-15 20:13:22 -0500

    fix(exceptions): Update for BindException handling

--- src/main/java/com/glaze/movieapi/exceptions/ApiExceptionHandler.java
+++ src/main/java/com/glaze/movieapi/exceptions/ApiExceptionHandler.java
@@ -1,6 +1,9 @@
 package com.glaze.movieapi.exceptions;
 
+import java.util.List;
 import java.util.Locale;
+import java.util.Map;
+import java.util.stream.Collectors;
 import jakarta.servlet.http.HttpServletRequest;
 
 import com.glaze.movieapi.utils.ExceptionHandlerUtils;
@@ -8,16 +11,17 @@ import org.springframework.context.MessageSource;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ProblemDetail;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindException;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @RestControllerAdvice
-public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
+public class ApiExceptionHandler {
     private final MessageSource source;
     public ApiExceptionHandler(MessageSource source) {
         this.source = source;
-        super.setMessageSource(source);
+        // super.setMessageSource(source);
     }
 
     @ExceptionHandler(value = NotFoundException.class)
@@ -32,6 +36,23 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
         problem.setTitle(detail);
 
         return ResponseEntity.status(HttpStatus.NOT_FOUND)
+            .body(problem);
+    }
+
+    @ExceptionHandler(value = BindException.class)
+    public ResponseEntity<ProblemDetail> handleBindException(BindException e) {
+        ProblemDetail problem = ProblemDetail.forStatus(HttpStatus.BAD_REQUEST);
+
+        Map<String, List<String>> errors = e.getFieldErrors()
+            .stream()
+            .collect(Collectors.groupingBy(
+                FieldError::getField,
+                Collectors.mapping(FieldError::getDefaultMessage, Collectors.toList())
+            ));
+
+        problem.setProperty("errors", errors);
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
             .body(problem);
     }
 
